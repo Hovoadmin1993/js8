210eec09a224478eab49301dfd7effa3dfbd93d5
Hovoadmin1993 <hovopetrosyan7@gmail.com>
2021-06-22 13:40:20 +0400

    json

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const url = "http://localhost:8000/interns";
+
+// Ajax
+
+// const xhr = new XMLHttpRequest();
+// xhr.open("get", "http://localhost:8000/interns", false);
+// xhr.send(null);
+// sa hin dzevn er ansinxron ashxatacnelu hamar
+
+// nor dzev@ 
+
+// fetch("http://localhost:8000/interns")
+// .then(response => response.json())
+// .then(data => console.log(data));
